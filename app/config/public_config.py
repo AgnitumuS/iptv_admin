de6c64899462f4e06427b3dcbd665c1340bdfbcb
topilski <atopilski@gmail.com>
2019-08-23 02:28:07 -0400

    AWS

--- app/config/public_config.py
+++ app/config/public_config.py
@@ -4,4 +4,4 @@ PUBLIC_CONFIG = {'site': {'title': 'FastoCloud', 'keywords': 'video,cloud,iptv,s
                              'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5',
                              'community_channel': 'https://discord.gg/cnUXsws',
                              'partners':[{'company': 'Tathya Technologies Pvt Ltd (OPC)', 'website': 'https://tathyatechnologies.com', 'country':'India, Bangladesh'}]},
-                 'project': {'version': '1.5.0', 'version_type': 'release'}}
+                 'project': {'version': '1.6.0', 'version_type': 'rc'}}
